c9d72fd936e1f7d6a25c0495ffca58a4433a25a8
Luhivskyi <luhivskyi.d@ukr.net>
2021-03-05 14:18:35 +0300

    hw-10-volume 2

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import './styles.css';
+import itemsTemplate from './templates/gallery.hbs'
+import menuItems from './menu.json'
+
+const themes = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+    galleryRef: document.querySelector('.js-menu'),
+    checkboxRef: document.querySelector('#theme-switch-toggle'),
+    bodyRef: document.querySelector('body'),
+}
+const { galleryRef, checkboxRef, bodyRef } = refs;
+
+window.addEventListener("load", getThemeFromLocalStorage)
+checkboxRef.addEventListener("change", changeTheme)
+
+function getThemeFromLocalStorage() {
+    const currentTheme = localStorage.getItem('theme')
+    if (currentTheme == 'dark-theme') {
+        bodyRef.classList.add(themes.DARK);
+        checkboxRef.setAttribute('checked', true)
+    } else {
+        bodyRef.classList.add(themes.LIGHT);
+    }
+
+}
+
+function changeTheme() {
+    if (checkboxRef.checked) {
+        bodyRef.classList.replace(themes.LIGHT, themes.DARK)
+        localStorage.setItem('theme', themes.DARK)
+    } else {
+        bodyRef.classList.replace(themes.DARK, themes.LIGHT)
+        localStorage.setItem('theme', themes.LIGHT)
+    }
+}
+
+function createMenu() {
+    const markup = itemsTemplate(menuItems);
+    galleryRef.insertAdjacentHTML('beforeend', markup)
+}
+createMenu();
